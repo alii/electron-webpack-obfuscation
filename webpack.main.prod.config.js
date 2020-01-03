5a4106db04c2032eb58292c714a52d711d8fb998
Alistair Smith <alistairsmith01+git@gmail.com>
2020-01-03 00:12:31 +0000

    Initial Commit

--- webpack.main.prod.config.js
+++ webpack.main.prod.config.js
@@ -1,6 +1,16 @@
 const merge = require('webpack-merge');
-
+const JavaScriptObfuscator = require('webpack-obfuscator');
 const baseConfig = require('./webpack.main.config');
+
+// Obfuscator
+baseConfig.plugins.push(new JavaScriptObfuscator({
+    rotateUnicodeArray: true,
+    compact: true,
+    deadCodeInjection: true,
+    deadCodeInjectionThreshold: 0.4,
+    sourceMap: false,
+    stringArrayEncoding: true,
+}));
 
 module.exports = merge.smart(baseConfig, {
     mode: 'production'
